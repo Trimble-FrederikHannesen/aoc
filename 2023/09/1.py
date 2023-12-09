2d7eb89701533d5edfd26f33f65026119163e74c
Frederik Hannesen <frederik_hannesen@trimble.com>
2023-12-09 12:32:50 +0100

    09

---
+++ 2023/09/1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+i = [[int(x) for x in l.split()] for l in open("input").read().splitlines() if l]
+
+def v(l):
+	if not any(l): return 0
+	return l[-1]+v([l[i+1]-l[i] for i in range(len(l)-1)])
+
+print(sum(v(l) for l in i))
